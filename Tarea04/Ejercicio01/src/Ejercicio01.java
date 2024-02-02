56467b2134cca18b60d8d4c9b760aff2853f31fd
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-02-02 14:00:02 +0100

    Actualización antes de ir a casa

---
+++ Tarea04/Ejercicio01/src/Ejercicio01.java
@@ -0,0 +1,15 @@
+public class Ejercicio01 {
+	public static void main(String[] args) {
+		int val1=8 , val2=5;
+		int suma=val1+val2;
+		int resta=val1-val2;
+		int multi=val1*val2;
+		int divi=val1/val2;
+		int modulo=val1%val2;
+		System.out.println(suma);
+		System.out.println(resta);
+		System.out.println(multi);
+		System.out.println(divi);
+		System.out.println(modulo);
+	}
+}
