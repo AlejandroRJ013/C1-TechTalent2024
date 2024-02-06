7e21cb6ce20b9ca1e9c066f493b5daa3d39669f4
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-02-06 13:53:25 +0100

    Update T5Ejercicio03.java

--- C2/Tarea05/Ejercicio03/src/T5Ejercicio03.java
+++ C2/Tarea05/Ejercicio03/src/T5Ejercicio03.java
@@ -1,3 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class T5Ejercicio03 {
-    
+    public static void main(String[] args) {
+        
+        String name=JOptionPane.showInputDialog("Introduzca su nombre:");
+        JOptionPane.showMessageDialog(null, "Bienvenido "+name.toUpperCase()+", esperamos verle por aqui");
+    }
 }
