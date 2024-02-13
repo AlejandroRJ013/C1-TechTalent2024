2dbb9ce153b9978de2532db7171c208d27442790
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-02-13 18:49:44 +0100

    retoque

--- C2/Tarea05/Ejercicio01/src/T5Ejercicio01.java
+++ C2/Tarea05/Ejercicio01/src/T5Ejercicio01.java
@@ -1,13 +1,15 @@
 public class T5Ejercicio01 {
 
     public static void main(String[] args) {
-        int val11=4523, val21=741;
-        String val1="\033[34m"+val11+"\u001B[0m", val2="\033[32m"+val21+"\u001B[0m";
-       
-        if (val11>val21) {
-            System.out.println("La variable val1: "+val1+" es mayor que val2: "+val2);
+        int val11 = 4523, val21 = 741;
+        String val1 = "\033[34m" + val11 + "\u001B[0m", val2 = "\033[32m" + val21 + "\u001B[0m";
+
+        if (val11 > val21) {
+            System.out.println("La variable val1: " + val1 + " es mayor que val2: " + val2);
+        } else if (val1 == val2) {
+            System.out.println("las dos números tienen el mismo valor");
         } else {
-            System.out.println("La variable val2: "+val2+" es mayor que val1: "+val1);
+            System.out.println("La variable val2: " + val2 + " es mayor que val1: " + val1);
         }
     }
 }
