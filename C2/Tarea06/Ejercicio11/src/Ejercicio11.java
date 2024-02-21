3b1b402e9537e03ba2d5455ddd412838e7015399
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-02-21 13:38:12 +0100

    clase

--- C2/Tarea06/Ejercicio11/src/Ejercicio11.java
+++ C2/Tarea06/Ejercicio11/src/Ejercicio11.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class Ejercicio11 {
     public static void main(String[] args) {
-        String tamaño = JOptionPane.showInputDialog("¿Cual será el tamaño de los arrays?");
+        String tamaño = JOptionPane.showInputDialog("¿Cual será el tamaño del array?");
         int tam = Integer.parseInt(tamaño);
         
         StringBuilder coleccion1 = new StringBuilder();
@@ -10,57 +10,48 @@ public class Ejercicio11 {
         StringBuilder coleccion3 = new StringBuilder();
         int array[] = new int[tam];
         int aleatorio[] = new int[tam];
-        int numsgen[] = new int[tam];
         int multiplicado[] = new int[tam];
-        int posicionale = (int) Math.random()*tam;
 
         rellenararray1(array, tam, coleccion1);
-        rellenararray2(array, aleatorio, posicionale, tam, numsgen);
-        rellenararray3(array, aleatorio, multiplicado, tam);
-
-        mostrar(coleccion1, coleccion2, coleccion3, array, aleatorio, multiplicado);
+        rellenararray2(array, aleatorio, tam, coleccion2);
+        rellenararray3(array, aleatorio, multiplicado, tam, coleccion3);
+        
+        JOptionPane.showMessageDialog(null, "La primera colección contiene: \n - "+coleccion1+" \n La segunda colección contiene: \n - "+coleccion2+" \n Y las multiplicaciones dan: \n - "+coleccion3);
     }
 
     public static void rellenararray1(int[] array, int tam, StringBuilder coleccion1) {
         for(int i=0; i<tam; i++) {
-            int alea = (int) Math.random()*100;
-            alea = array[i];
+            int alea = (int) Math.random()*50;
+            array[1] = alea;
             coleccion1.append(alea+" ");
         }
     }
 
-    public static void rellenararray2(int[] array, int[] aleatorio, int posicionale, int tam, int[] numsgen) {
+    public static void rellenararray2(int[] array, int[] aleatorio, int tam, StringBuilder coleccion2) {
+        int posicionale = (int) Math.random()*tam;
+        
         for (int i = 0; i < tam; i++) {
-            for (int esta : numsgen) {
-                while (posicionale == esta) {
-                    posicionale = (int) Math.random()*tam;
-                }
+            if (repes(aleatorio, posicionale)) {
+                posicionale = (int) Math.random()*tam;
             }
-            numsgen[i] = posicionale;
             aleatorio[i] = array[posicionale];
+            coleccion2.append(aleatorio[i]+" ");
         }
     }
 
-    public static void rellenararray3(int[] array, int[] aleatorio, int[] multiplicado, int tam) {
+    public static boolean repes(int[] valor, int aleatorio) {
+        for (int preguntorepe : valor) {
+            if (preguntorepe == aleatorio) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void rellenararray3(int[] array, int[] aleatorio, int[] multiplicado, int tam, StringBuilder coleccion3) {
         for (int i = 0; i < tam; i++) {
             multiplicado[i] = array[i]*aleatorio[i];
+            coleccion3.append(multiplicado[i]+" ");
         }
-    }
-    
-    public static void mostrar(StringBuilder coleccion1, StringBuilder coleccion2, StringBuilder coleccion3, int[] array, int[] aleatorio, int[] multiplicado) {
-        for (int num : array) {
-            coleccion1.append(num+" ");
-        }
-        String arraystr = coleccion1.toString();
-        for (int numale : aleatorio) {
-            coleccion2.append(numale+" ");
-        }
-        String aleatoriostr = coleccion1.toString();
-        for (int mul : multiplicado) {
-            coleccion3.append(mul+" ");
-        }
-        String multiplicadostr = coleccion1.toString();
-
-        JOptionPane.showMessageDialog(null, "La primera colección contiene: \n - "+arraystr+" \n La segunda colección contiene: \n - "+aleatoriostr+" \n Y las multiplicaciones dan: \n - "+multiplicadostr);
     }
 }
