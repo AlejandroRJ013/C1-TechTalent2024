dee6bb58dcf256fa9f16e39ff9144a30097f04ba
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-03-05 13:40:42 +0100

    trabajo grupo

--- C2/Tarea07/PruebasGrupo2.java
+++ C2/Tarea07/PruebasGrupo2.java
@@ -7,18 +7,16 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class PruebasGrupo2 {
     public static void main(String[] args) {
         StringBuilder datos = new StringBuilder();
-        StringBuilder grupo1 = new StringBuilder("\nGrupo 1: \n");
-        StringBuilder grupo2 = new StringBuilder("\nGrupo 2: \n");
-        StringBuilder grupo3 = new StringBuilder("\nGrupo 3: \n");
-        StringBuilder grupo4 = new StringBuilder("\nGrupo 4: \n");
-        StringBuilder grupo5 = new StringBuilder("\nGrupo 5: \n");
+        StringBuilder grupo1 = new StringBuilder();
+        StringBuilder grupo2 = new StringBuilder();
+        StringBuilder grupo3 = new StringBuilder();
+        StringBuilder grupo4 = new StringBuilder();
+        StringBuilder grupo5 = new StringBuilder();
         HashMap<Integer, String> idNombre = new HashMap<>();
         HashMap<Integer, Integer> idEdad = new HashMap<>();
         AtomicBoolean ordenarID = new AtomicBoolean(false);
         AtomicBoolean ordenarNombre = new AtomicBoolean(false);
         AtomicBoolean ordenarEdad = new AtomicBoolean(false);
-
-        // int cant = Integer.parseInt(JOptionPane.showInputDialog("Cantidad"));
 
         idNombre.put(1, "Jose");
         idNombre.put(2, "Isabel");
@@ -27,20 +25,20 @@ public class PruebasGrupo2 {
         idNombre.put(5, "Abdellah");
         idNombre.put(6, "Antonio");
         idNombre.put(7, "Diego");
-        idNombre.put(8, "Manel");
+        idNombre.put(8,"Manel");
         idNombre.put(9, "Jessica");
         idNombre.put(10, "Santos");
         idNombre.put(11, "Alex");
-        idNombre.put(12, "Ana María");
+        idNombre.put(12, "Ana_María");
         idNombre.put(13, "Aurora");
         idNombre.put(14, "Laia");
         idNombre.put(15, "Alejandro");
 
+        // int cant = Integer.parseInt(JOptionPane.showInputDialog("Cantidad"));
+
         // int ultimoID = 0;
         // for (int i = 1; i <= cant; i++) {
-        // JPanel panel = new JPanel(new GridLayout(0, 2)); // GridLayout para organizar
-        // los campos de texto en dos
-        // // columnas
+        // JPanel panel = new JPanel(new GridLayout(0, 2)); 
         // panel.add(new JLabel("Nombre: "));
         // JTextField nombre = new JTextField(10);
         // panel.add(nombre);
@@ -49,8 +47,7 @@ public class PruebasGrupo2 {
         // JTextField edad = new JTextField(10);
         // panel.add(edad);
 
-        // int result = JOptionPane.showConfirmDialog(null, panel, "Introduzca los datos
-        // del alumno",
+        // int result = JOptionPane.showConfirmDialog(null, panel, "Introduzca los datos del alumno",
         // JOptionPane.OK_CANCEL_OPTION);
 
         // if (result == JOptionPane.OK_OPTION) {
@@ -62,16 +59,15 @@ public class PruebasGrupo2 {
         // ultimoID = i;
         // }
 
-        for (Integer id : idNombre.keySet()) {
-            datos.append("Nombre: " + idNombre.get(id) + "\n - ID: " + id + "\n - Edad: " + idEdad.get(id));
+        // for (Integer id : idNombre.keySet()) {
+        //     datos.append("Nombre: " + idNombre.get(id) + "\n - ID: " + id + "\n - Edad: " + idEdad.get(id));
 
-            System.out.println("ID: " + id /* + " ULTIMO id: " + ultimoID */);
-            System.out.println(" - Nombre: " + idNombre.get(id) + "\n - Edad: " + idEdad.get(id));
+        //     System.out.println("ID: " + id /* + " ULTIMO id: " + ultimoID */);
+        //     System.out.println(" - Nombre: " + idNombre.get(id) + "\n - Edad: " + idEdad.get(id));
 
-        }
-        System.out.println("1");
+        // }
+
         marcadores(datos, ordenarID, ordenarNombre, ordenarEdad);
-        System.out.println("2");
 
         if (ordenarID.get()) {
             System.out.println("Ordenar por ID");
@@ -184,7 +180,7 @@ public class PruebasGrupo2 {
             AtomicBoolean ordenarID, AtomicBoolean ordenarNombre,
             AtomicBoolean ordenarEdad) {
         JFrame frame = new JFrame("Alumnos curso TechTalent2024");
-        frame.setSize(600, 300);
+        frame.setSize(450, 155);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -195,19 +191,50 @@ public class PruebasGrupo2 {
         titulo.setHorizontalAlignment(SwingConstants.CENTER); // Mover el texto al centro en la posición horizontal
         titul.add(titulo);
 
-        JPanel contenidoDinamico = new JPanel(); // Creamos un panel donde iremos poniendo varios paneles según que
+        JPanel contenidoDinamico = new JPanel(new GridLayout(1, 5)); // Creamos un panel donde iremos poniendo varios paneles según que
                                                  // quiero mostrar
-        contenidoDinamico.setBounds(0, 50, 600, 285); // Establezco un tamaño y posición fijo
+        contenidoDinamico.setBounds(0, 30, 440, 150); // Establezco un tamaño y posición fijo
         JPanel panelGrupo1 = new JPanel();
-        panelGrupo1.setBounds(0, 50, 120, 285);
         JPanel panelGrupo2 = new JPanel();
-        panelGrupo2.setBounds(120, 50, 120, 285);
         JPanel panelGrupo3 = new JPanel();
-        panelGrupo3.setBounds(240, 50, 120, 285);
         JPanel panelGrupo4 = new JPanel();
-        panelGrupo4.setBounds(360, 50, 120, 285);
         JPanel panelGrupo5 = new JPanel();
-        panelGrupo5.setBounds(480, 50, 120, 285);
+
+        JLabel grupo1Label = new JLabel();
+        grupo1Label.setText("/ GRUPO 1 /\n");
+        JPanel integrantesG1 = new JPanel();
+        integrantesG1.setSize(120, 150);
+        
+        panelIntegrantes(grupo1, integrantesG1);
+        
+        JLabel grupo2Label = new JLabel();
+        grupo2Label.setText("/ GRUPO 2 /\n");
+        JPanel integrantesG2 = new JPanel();
+        integrantesG2.setSize(120, 150);
+        
+        panelIntegrantes(grupo2, integrantesG2);
+
+        JLabel grupo3Label = new JLabel();
+        grupo3Label.setText("/ GRUPO 3 /\n");
+        JPanel integrantesG3 = new JPanel();
+        integrantesG3.setSize(120, 150);
+        
+        panelIntegrantes(grupo3, integrantesG3);
+
+        JLabel grupo4Label = new JLabel();
+        grupo4Label.setText("/ GRUPO 4 /\n");
+        JPanel integrantesG4 = new JPanel();
+        integrantesG4.setSize(120, 150);
+        
+        panelIntegrantes(grupo4, integrantesG4);
+
+        JLabel grupo5Label = new JLabel();
+        grupo5Label.setText("/ GRUPO 5 /\n");
+        JPanel integrantesG5 = new JPanel();
+        integrantesG5.setSize(120, 150);
+        
+        panelIntegrantes(grupo5, integrantesG5);
+
 
         if (ordenarID.get()) {
             panelGrupo1.add(new JLabel(grupo1.toString()));
@@ -216,12 +243,20 @@ public class PruebasGrupo2 {
             panelGrupo4.add(new JLabel(grupo4.toString()));
             panelGrupo5.add(new JLabel(grupo5.toString()));
         } else if (ordenarNombre.get()) {
-            gruposAlfabetizados(idNombre, grupo1, grupo2, grupo3, grupo4, grupo5);
-            panelGrupo1.add(new JLabel(grupo1.toString()));
-            panelGrupo2.add(new JLabel(grupo2.toString()));
-            panelGrupo3.add(new JLabel(grupo3.toString()));
-            panelGrupo4.add(new JLabel(grupo4.toString()));
-            panelGrupo5.add(new JLabel(grupo5.toString()));
+            panelGrupo1.add(grupo1Label);
+            panelGrupo1.add(integrantesG1);
+            
+            panelGrupo2.add(grupo2Label);
+            panelGrupo2.add(integrantesG2);
+            
+            panelGrupo3.add(grupo3Label);
+            panelGrupo3.add(integrantesG3);
+            
+            panelGrupo4.add(grupo4Label);
+            panelGrupo4.add(integrantesG4);
+            
+            panelGrupo5.add(grupo5Label);
+            panelGrupo5.add(integrantesG5);
         } else if (ordenarEdad.get()) {
             panelGrupo1.add(new JLabel(grupo1.toString()));
             panelGrupo2.add(new JLabel(grupo2.toString()));
@@ -250,17 +285,14 @@ public class PruebasGrupo2 {
             ordenAlfa.add(nombre);
         }
 
-        System.out.println(ordenAlfa.toString());
         Collections.sort(ordenAlfa);
-        System.out.println(ordenAlfa.toString());
 
         int numeroGrupo = 1;
         int integrantesGrupo = 1;
         for (int indice = 0; indice < ordenAlfa.size(); indice++) {
             if (integrantesGrupo <= 2) {
                 String nombre = ordenAlfa.get(indice);
-                System.out.println(ordenAlfa.get(indice));
-                grup.append(nombre + ", ");
+                grup.append(nombre + " ");
             } else if (integrantesGrupo == 3) {
                 String nombre = ordenAlfa.get(indice);
                 grup.append(nombre);
@@ -285,6 +317,16 @@ public class PruebasGrupo2 {
             }
 
             integrantesGrupo++;
+        }
+    }
+
+    public static void panelIntegrantes(StringBuilder integrantesGrupo, JPanel losIntegrantes) {
+    
+        losIntegrantes.setLayout(new BoxLayout(losIntegrantes, BoxLayout.Y_AXIS));
+        String[] integrantes = integrantesGrupo.toString().split(" ");
+        for (String integrante : integrantes) {
+            JLabel labelIntegrante = new JLabel(" - " + integrante);
+            losIntegrantes.add(labelIntegrante);
         }
     }
 }
