402079925aafb7e0545789ecd677ddd547cbfd84
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-03-11 02:19:48 +0100

    Create Ejercicio04.java

---
+++ C2/Tarea07/Ejercicio04.java
@@ -0,0 +1,12 @@
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        /*
+         * Hacer la interfaz del ejercicio 3 pero poner un checkbox a cada producto,
+         * añadir un boton a la barra de botones {lo que hara este boton es que cuando
+         * se pulse recogera todos los checkbox seleccionados, preguntara la cantidad
+         * que se va a comprar de un producto(luego otro, y el siguiente, etc.), la
+         * cantidad comprada se multiplica por el precio del producto y se resta
+         * la cantidad que se a comprado del stock que habia del producto}
+         */
+    }
+}
