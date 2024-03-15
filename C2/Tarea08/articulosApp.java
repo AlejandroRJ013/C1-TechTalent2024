134a1ec588db7026af957d42e230928ac74f8a05
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-03-15 13:48:38 +0100

    ejer08

--- C2/Tarea08/articulosApp.java
+++ C2/Tarea08/articulosApp.java
@@ -2,14 +2,8 @@ public class articulosApp {
     public class articulos {
 
         //	ATRIBUTOS
-        String nombre;
-        double precioBruto;
-        int cantidad;
-
         String producto;
         int cantidadProducto;
-        boolean masProductos;
-        boolean esencal;
 
         //CONTRUSCTORES
         public articulos() {
@@ -17,53 +11,10 @@ public class articulosApp {
             this.cantidadProducto = 0;
         }
         
-        public articulos(String producto, int cantidadProducto, boolean masProductos, boolean esencal) {
-            this.nombre = producto;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidadProducto;
-        }
-        
-        public articulos(String nombre, double precioBruto, int cantidad) {
-            this.nombre = nombre;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidad;
-        }
+        public articulos(String producto, int cantidadProducto) {
+            this.producto = producto;
+            this.cantidadProducto = cantidadProducto;
 
-        public articulos(String nombre, int cantidad, double precioBruto) {
-            this.nombre = nombre;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidad;
-        }
-        
-        public articulos(double precioBruto, int cantidad, String nombre) {
-            this.nombre = nombre;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidad;
-        }
-        
-        public articulos(double precioBruto, String nombre, int cantidad) {
-            this.nombre = nombre;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidad;
-        }
-
-        public articulos(int cantidad, String nombre, double precioBruto) {
-            this.nombre = nombre;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidad;
-        }
-    
-        public articulos(int cantidad, double precioBruto, String nombre) {
-            this.nombre = nombre;
-            this.precioBruto = precioBruto;
-            this.cantidad = cantidad;
-        }
-
-        if (masArticulos.isSelected()) {
-            masProductos = true;
-        }
-        if (articuloEsencial.isSelected()) {
-            esencal = true;
         }
     }
 }
